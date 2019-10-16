648a24a1f0b2c6050c4286f5b731bedb0182be5f
Wolfgang Fahl <wf@bitplan.com>
2019-10-16 12:26:42 +0200

    adds polyglot to install and improves takePicture

--- Camera.py
+++ Camera.py
@@ -12,32 +12,42 @@ class Camera:
 
   # construct me
   def __init__(self):
+    self.webcam=None
     # need to check whetheer PiCamera is available
     # https://www.raspberrypi.org/forums/viewtopic.php?t=46113
     try:
       self.picam = PiCamera()
       self.cam=picam
-      self.webcam=None
     except PiCameraError as e:
       self.picam=None
-      self.webcam = cv2.VideoCapture(0)
-      self.cam=self.webcam
+      self.getWebCam()
       if not self.webcam:
         print ("Camera failure: %s" % (str(e)))
 
+  def getWebCam(self):
+      if self.webcam is not None:
+        # https://stackoverflow.com/a/44455417/1497139
+        self.webcam.release()
+      self.webcam=cv2.VideoCapture(0)
+      self.cam=self.webcam
+      return self.webcam
+
   # take a picture
-  def takePicture(self):
+  def takePicture(self,height=400,width=400):
     if self.picam:
       image=self.takePictureFromPiCam()
     elif self.webcam:
+      self.getWebCam()
       ret,image=self.webcam.read()
       if not ret:
         raise Exception('webcam image read failed')
     else:
       raise Exception('no camera available')
+    resized = imutils.resize(image.copy(), height = height, width = width)
     if Camera.debug:
-      cv2.imshow('Camera',image)
-    return image
+      cv2.imshow('camera',image)
+      cv2.imshow('resized',resized)
+    return resized 
 
   def takePictureFromPiCam(self):
     # initialize the camera and grab a reference to the raw camera capture
@@ -49,9 +59,6 @@ class Camera:
     # grab an image from the camera
     self.cam.capture(rawCapture, format="bgr")
     image = rawCapture.array
-    resized = imutils.resize(image, height = 400, width = 400)
-
-
     return resized
 
   def close(self):
